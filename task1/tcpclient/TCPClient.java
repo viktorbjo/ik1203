cd3a0d21e5032cfa75818e42fdcd4e1e44de8daa
viktorbjo <viktor.kung@live.se>
2023-02-07 16:18:17 +0100

    edits

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -9,21 +9,21 @@ public class TCPClient {
 
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
 
-        ByteArrayOutputStream response = new ByteArrayOutputStream(); //skapa en ByteArrayOutputStream för att spara det som hämats från servern
+        ByteArrayOutputStream out = new ByteArrayOutputStream(); //skapa en ByteArrayOutputStream för att spara det som hämats från servern
         Socket clientSocket = new Socket(hostname, port);            // initsiera en socket
         InputStream inFromServer = clientSocket.getInputStream();   //InputStream för att ta emot data från server
         OutputStream outToServer = clientSocket.getOutputStream(); // Samma som ovan fast skicka
 
         outToServer.write(toServerBytes); // skickar data från toServerBytes till outputStream
 
-        byte[] buffer = new byte[1024]; // alokera minnes buffer
+        byte[] buffer = new byte[1024]; // alokera statisk minnes buffer
 
         for (int length = inFromServer.read(buffer); length > 0; length = inFromServer.read(buffer)) { // loopar för att skriva den data vi tar emot till byteArrayOutputStream
-            response.write(buffer, 0, buffer.length);
+            out.write(buffer, 0, buffer.length);
         }
 
-        clientSocket.close();
-        return response.toByteArray();
+        clientSocket.close(); //stäng av socket anslutningen
+        return out.toByteArray();
 
     }
 
