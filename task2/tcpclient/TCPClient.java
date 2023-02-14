25f5838049333e8f5d809d71c459cf27b1dea232
viktorbjo <viktor.kung@live.se>
2023-02-14 10:24:09 +0100

    task 2

---
+++ task2/tcpclient/TCPClient.java
@@ -0,0 +1,13 @@
+package tcpclient;
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    
+    public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+    }
+
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+        return new byte[0];
+    }
+}
