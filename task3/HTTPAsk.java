fe51a34b5a571e587bc64a1fb2df8ce3b11d0e5c
viktorbjo <viktor.kung@live.se>
2023-02-24 10:20:30 +0100

    task3 funka typ

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -1,62 +1,78 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import tcpclient.TCPClient;
 
-public class HTTPAsk {
-    public static void main( String[] args) {
+public class HTTPAsk { 
+ public static void main(String[] args) throws Exception {
+        // Parse command-line arguments
+       Integer port = Integer.parseInt(args[0]);
 
-      try {
-            int port = Integer.parseInt(args[0]);
-            ServerSocket serverSocket = new ServerSocket(port);
-            while (true) {
-                Socket clientSocket = serverSocket.accept(); // Wait for incoming connections
+        // Create server socket
+        ServerSocket serverSocket = new ServerSocket(port);
 
-                // Read input from client
-                InputStream inputStream = clientSocket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String request = reader.readLine();
+        // Initialize variables for handling incoming requests
+        boolean shutdown = false;
+        Integer timeout = null; // milliseconds
+        Integer limit = null;
+        String hostname = null;
+        byte[] data = new byte[0];
+	  while(true) {
+        try (
+			 Socket socket = serverSocket.accept();
+       DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+       InputStream in = socket.getInputStream()){
 
-                // Parse input
-                String[] requestParts = request.split("[ ?&]"); // Split request on spaces, question marks and ampersands
-                String hostname = null;
-                int portNum = 0;
-                String command = null;
-                String[] parameters = new String[0];
-                for (int i = 0; i < requestParts.length; i++) {
-                    if (requestParts[i].equals("hostname")) {
-                        hostname = requestParts[i + 1];
-                    } else if (requestParts[i].equals("port")) {
-                        portNum = Integer.parseInt(requestParts[i + 1]);
-                    } else if (requestParts[i].equals("string")) {
-                        command = requestParts[i + 1];
-                    } else if (requestParts[i].equals("verbose")) {
-                        parameters = new String[] { "verbose" };
-                    }
-                }
+            byte[] request = new byte[1024];
+            int read = in.read(request);
+            String requestString = new String(request, 0, read);
 
-                String message = parameters.length > 4 ? parameters[5] : "";
-                // Call TCPClient and get response
-                TCPClient tcpClient = new TCPClient(false, 5000, 1024);
-                byte[] responseBytes = new byte[0];
-                try {
-                    responseBytes = tcpClient.askServer(hostname, portNum, message.getBytes("UTF-8"));
-                } catch (IOException e) {
-                    responseBytes = "HTTP/1.1 404 Not Found\r\n\r\n".getBytes("UTF-8");
-                }
+           
+            String[] lines = requestString.split("\r\n");
+            String[] words = lines[0].split(" ");
+            String[] parameters = words[1].split("[?&]");
+ 
 
-                // Send response to client
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes("UTF-8"));
-                outputStream.write(responseBytes);
-
-                // Close socket and streams
-                outputStream.close();
-                inputStream.close();
-                reader.close();
-                clientSocket.close();
-            }
-        } catch (IOException e) {
-            System.err.println("Error: " + e.getMessage());
+    for (int i = 1; i < parameters.length; i++ ) {
+        String[] parameter = parameters[i].split("=");
+        switch (parameter[0]) {
+            case "hostname":
+                hostname = parameter[1];
+                break;
+            case "port":
+                port = Integer.parseInt(parameter[1]);
+                break;
+            case "limit":
+                limit = Integer.parseInt(parameter[1]);
+                break;
+            case "string":
+                data = parameter[1].getBytes();
+                break;
+            case "timeout":
+                timeout = Integer.parseInt(parameter[1]);
+                break;
+            case "shutdown":
+                shutdown = Boolean.parseBoolean(parameter[1]);
+                break;
+            default:
+                break;
         }
     }
-}
+
+
+        TCPClient tcpClient = new TCPClient(shutdown, timeout, limit);
+        byte[] toClientBytes = tcpClient.askServer(hostname, port, data);
+        ByteArrayOutputStream serverOutput = new ByteArrayOutputStream();
+        serverOutput.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        serverOutput.write(toClientBytes);
+        byte[] responseHeader = serverOutput.toByteArray();
+        out.write(responseHeader);
+            }
+            catch(Exception e){
+                System.out.println("Exception thrown: " + e);
+        }
+ }
+ }
+ }
+
+
